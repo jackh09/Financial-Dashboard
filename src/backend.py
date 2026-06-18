ce22e0cf2992a6b0d05b7779c69a3d5198d12504
Jack Hughes <jack.hughes2402@gmail.com>
2026-06-18 13:14:36 +0100

    coded up backend: all left to do is while loop every minute and get search query

--- src/backend.py
+++ src/backend.py
@@ -16,31 +16,54 @@ def getSearchQuery():
 
 query = "TSLA"
 
-## 2. Create yfinance ticker ##
+"""
+This function fetches all of the stock data and puts it into the JSON file.
+"""
+def getStockData(stock: str, timePeriod: str):
+    ## 2. Create yfinance ticker ##
+    stockTicker = yf.Ticker(stock)
 
-stockTicker = yf.Ticker(query)
+    ## 3. Get price history ##
+    priceHistory = stockTicker.history(period=timePeriod)   # get the history for 1 month
+    priceHistory = priceHistory.reset_index()               # reset the indexes to make it formattable
 
-## 3. Get price history ##
+    ## 4. Extra information ##
+    extraInfoData = stockTicker.fast_info       # download database of all information today
 
-priceHistory = stockTicker.history(period="1m")         # get the history for 1 month
-priceHistory = priceHistory.reset_index()               # reset the indexes to make it formattable
+    # Assign each variable a unique datapoint
+    openingPrice = extraInfoData.get("open")
+    prevClosingPrice = extraInfoData.get("previousClose")
+    dailyHigh = extraInfoData.get("dayHigh")
+    dailyLow = extraInfoData.get("dayLow")
+    volume = extraInfoData.get("lastVolume")
+    marketCap = extraInfoData.get("marketCap")
 
-## 4. Extra information ##
+    ## 5. Daily mean return using pandas ##
+    dailyReturns = priceHistory["Close"].pct_change()
+    meanDailyReturn = dailyReturns.mean()
 
-extraInfoData = stockTicker.fast_info       # download database of all information today
+    ## 6. Pack data into JSON ##
+    historyJSON = priceHistory.to_json(orient="records", date_format="iso") # Raw data -> JSON string
+    historyData = json.loads(historyJSON) # JSON string -> list
 
-# Assign each variable a unique datapoint
-openingPrice = extraInfoData.get("open")
-prevClosingPrice = extraInfoData.get("previousClose")
-dailyHigh = extraInfoData.get("dayHigh")
-dailyLow = extraInfoData.get("dayLow")
-volume = extraInfoData.get("lastVolume")
-marketCap = extraInfoData.get("marketCap")
+    JSONPackage = {
+        "metadata": {
+            "ticker": stock,
+            "open": openingPrice,
+            "previousClose": prevClosingPrice,
+            "high": dailyHigh,
+            "low": dailyLow,
+            "volume": volume,
+            "marketCap": marketCap,
+            "meanDailyReturn": meanDailyReturn
+        },
+        "history": historyData
+    }
 
-## 5. Daily mean return using pandas ##
+    # Write to JSON
+    with open("data.json", "w") as file:
+        json.dump(JSONPackage, file, indent=4)
 
-dailyReturns = priceHistory["close"].pct_change()
-meanDailyReturn = dailyReturns.mean()
 
-
+getStockData(query, "1mo")
 
